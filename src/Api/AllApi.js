94bc9f38a237ec610ff8a0c26eb3fe745e36e8d5
Nurkhankazi <nurkhan7494@gmail.com>
2024-10-01 13:25:18 +0600

    login add (laravel)

--- src/Api/AllApi.js
+++ src/Api/AllApi.js
@@ -2,29 +2,41 @@ import axios from 'axios';
 import {useNavigate } from "react-router-dom";
 
 const login = async (data) => {
-    const LOGIN_ENDPOINT = `http://localhost/inventory/login.php`;
+    const LOGIN_ENDPOINT = `${process.env.REACT_APP_API_URL}/login`;
     try{
-        let response = await axios.post(LOGIN_ENDPOINT, data);
-        if(response.data.jwt){
-            localStorage.setItem("access_token", response.data.jwt);
-            localStorage.setItem("userdata", response.data.datas);
+        let response= await axios({
+            method: 'post',
+            url: LOGIN_ENDPOINT,
+            data: data
+        });
+
+        if(response.data.data.token){
+            localStorage.setItem("access_token", response.data.data.token);
+            localStorage.setItem("userdata", response.data.data.data);
             return true;
         }else{
             return false;
         }
     }catch(e){
-        return false;
+       // return false;
         console.log(e);
     }
 }
 const register = async (data)=> {
-    const SIGNUP_ENDPOINT = `http://localhost/inventory/register.php`;
+    const SIGNUP_ENDPOINT = `${process.env.REACT_APP_API_URL}/register`;
     try{
         let response= await axios({
             method: 'post',
             responsiveTYpe: 'json',
             url: SIGNUP_ENDPOINT,
-            data: data
+            data: data,
+            withCredentials:true,
+            headers:{
+                Accept:"application/json",
+                "X-Request-With":"XMLHttpRequest",
+                'Access-Control-Allow-origin':'*',
+                "Content-Type":'multipart/form-data'
+            }
         });
         console.log(response);
     } 
@@ -36,4 +48,5 @@ const logout = ()=>{
     localStorage.removeItem("access_token");
     localStorage.removeItem("userdata");
 }
-export  { login, register, logout }
+
+export  { login, register, logout  }
