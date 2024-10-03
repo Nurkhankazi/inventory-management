e901cd3d4e392689438095af49fba3e8218d9283
Nurkhankazi <nurkhan7494@gmail.com>
2024-10-03 13:54:45 +0600

    api update

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,8 +1,9 @@
-import React,{useState} from 'react';
-import AuthLayout from '../../layouts/AuthLayout';
-import { Link,useNavigate } from 'react-router-dom';
+import React,{useState} from "react";
+import AuthLayout from "../../layouts/AuthLayout";
+import { Link, useNavigate, Navigate } from 'react-router-dom';
 import { login } from '../../Api/AllApi';
-function Login() {
+
+function Login(){
     const navigate = useNavigate();
     const [inputs, setInputs ] = useState([]);
     const handleChange = (event) => {
@@ -10,58 +11,62 @@ function Login() {
         const value = event.target.value;
         setInputs(values =>({...values, [name]: value}))
     }
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         let check = await login(inputs);
+        console.log(check)
         if(check){
             window.location=process.env.REACT_APP_BASE_URL
         }else{
             alert("Sorry password or email address is wrong!");
         }
     }
-  return (
-    <AuthLayout>
-        <div className="text-center mb-5">
-            <img src="./assets/img/favicon/favicon.ico" height="48" className='mb-4'/>
-            <h3>Sign In</h3>
-            <p>Please sign in to continue to Voler.</p>
+    return(
+        <AuthLayout>
+            
+            <div className="text-center mb-5">
+        <img src="./assets/img/favicon/favicon.ico" height="48" className='mb-4'/>
+        <h3>Sign In</h3>
+        <p>Please sign in to continue to HMS.</p>
+    </div>
+    <form onSubmit={handleSubmit}>
+        <div className="form-group position-relative has-icon-left">
+            <label htmlFor="email">Email</label>
+            <div className="position-relative">
+                <input type="email" className="form-control" id="email" name="email" onChange={handleChange}/>
+                <div className="form-control-icon">
+                    <i data-feather="user"></i>
+                </div>
+            </div>
         </div>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group position-relative has-icon-left">
-                <label htmlFor="email">Email</label>
-                <div className="position-relative">
-                    <input type="email" name='email' className="form-control" id="email" onChange={handleChange}/>
-                    <div className="form-control-icon">
-                        <i data-feather="user"></i>
-                    </div>
-                </div>
-            </div>
-            <div className="form-group position-relative has-icon-left">
-                <div className="clearfix">
-                    <label htmlFor="password">Password</label>
-                    <a href="auth-forgot-password.html" className='float-right'>
-                        <small>Forgot password?</small>
-                    </a>
-                </div>
-                <div className="position-relative">
-                    <input type="password" name='password' className="form-control" id="password" onChange={handleChange}/>
-                    <div className="form-control-icon">
-                        <i data-feather="lock"></i>
-                    </div>
-                </div>
-            </div>
-
-            <div className='form-check clearfix my-4'>
-                <div className="float-right">
-                    <Link to="/register">Don't have an account?</Link>
-                </div>
-            </div>
+        <div className="form-group position-relative has-icon-left">
             <div className="clearfix">
-                <button className="btn btn-primary float-right">Submit</button>
+                <label htmlFor="password">Password</label>
+                <a href="auth-forgot-password.html" className='float-right'>
+                    <small>Forgot password?</small>
+                </a>
             </div>
-        </form>
-    </AuthLayout>
-  )
-}
+            <div className="position-relative">
+                <input type="text" className="form-control" id="password" name="password" onChange={handleChange}/>
+                <div className="form-control-icon">
+                    <i data-feather="lock"></i>
+                </div>
+            </div>
+        </div>
 
-export default Login
+        <div className='form-check clearfix my-4'>
+           
+            <div className="float-right">
+                <Link to="/register">Don't have an account?</Link> 
+            </div>
+        </div>
+        <div className="clearfix">
+            <button className="btn btn-primary float-right">Submit</button>
+        </div>
+    </form>
+  
+        </AuthLayout>
+    )
+}
+export default Login ;
--- src/Pages/Products_Add/index.js
+++ src/Pages/Products_Add/index.js
@@ -1,48 +1,133 @@
-import React from 'react'
-import AdminLayout from '../../layouts/AdminLayout'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import AdminLayout from '../../layouts/AdminLayout';
+import { useNavigate } from 'react-router-dom';
+import {useParams} from "react-router-dom";
 
-function Products_Add() {
+function Addproduct() {
+    const [inputs, setInputs] = useState({addproduct:'',description:'',quantity:'',price:'',category:''});
+    const navigate=useNavigate();
+    const {id} = useParams();
     
+    function getDatas(){
+        axios.get(`${process.env.REACT_APP_API_URL}/addproduct/${id}`).then(function(response) {
+            setInputs(response.data.data);
+        });
+    }
+
+    useEffect(() => {
+        if(id){
+            getDatas();
+        }
+    }, []);
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        console.log(inputs)
+        
+        try{
+            let apiurl='';
+            if(inputs.id!=''){
+                apiurl=`/add_products/${inputs.id}`;
+            }else{
+                apiurl=`/add_products/create`;
+            }
+            
+            let response= await axios({
+                method: 'post',
+                responsiveTYpe: 'json',
+                url: `${process.env.REACT_APP_API_URL}${apiurl}`,
+                data: inputs
+            });
+            navigate('/add_products')
+        } 
+        catch(e){
+            console.log(e);
+        }
+    }
   return (
     <AdminLayout>
-         <div><br/>
-         <h2 className="panel-title" style={{textAlign:'center'}}>Products Add</h2>
-          <form >
-            <div class="mb-3">
-                <label for="Product_Name" class="form-label">Product Name</label>
-                <input type="text" class="form-control" id="Product_Name" required/>
+        <div className="main-content container-fluid">
+            <div className="page-title">
+                <div className="row">
+                    <div className="col-12 col-md-6 order-md-1 order-last">
+                        <h3>Add New Product</h3>
+                    </div>
+                    <div className="col-12 col-md-6 order-md-2 order-first">
+                        <nav aria-label="breadcrumb" className='breadcrumb-header'>
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                                <li className="breadcrumb-item active" aria-current="page">Add New</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
             </div>
 
-            <div class="mb-3">
-                <label for="itemDescription" class="form-label">Description</label>
-                <textarea class="form-control" id="itemDescription" rows="3" required></textarea>
-            </div>
+            <section id="basic-vertical-layouts">
+                <div className="row match-height">
+                    <div className="col-12">
+                        <div className="card">
+                            <div className="card-content">
+                                <div className="card-body">
+                                    <form className="form form-vertical" onSubmit={handleSubmit}>
+                                        <div className="form-body">
+                                            <div className="row">
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="first-name-vertical">Add Product</label>
+                                                    <input type="text" id="first-name-vertical" className="form-control" defaultValue={inputs.Add_Product} name="name" onChange={handleChange} placeholder="Add Product"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">description</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.description} name="designation_id" onChange={handleChange} placeholder="description"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">quantity</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.quantity} name="department_id" onChange={handleChange} placeholder="quantity"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">price</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.price} name="specialist" onChange={handleChange} placeholder="price"/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-12">
+                                                    <div className="form-group">
+                                                    <label for="email-id-vertical">category</label>
+                                                    <input type="text" id="email-id-vertical" className="form-control" defaultValue={inputs.category} name="education" onChange={handleChange} placeholder="category"/>
+                                                    </div>
+                                                </div>
+                                                
+                                                
+                                                <div className="col-12 d-flex justify-content-end">
+                                                    <button type="submit" className="btn btn-primary mr-1 mb-1">Submit</button>
+                                                    <button type="reset" className="btn btn-light-secondary mr-1 mb-1">Reset</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
 
-            <div class="mb-3">
-                <label for="itemQuantity" class="form-label">Quantity</label>
-                <input type="number" class="form-control" id="itemQuantity" required/>
-            </div>
-
-            <div class="mb-3">
-                <label for="itemPrice" class="form-label">Price</label>
-                <input type="number" class="form-control" id="itemPrice" step="0.01" required/>
-            </div>
-
-            <div class="mb-3">
-                <label for="itemCategory" class="form-label">Category</label>
-                <select class="form-select" id="itemCategory" required>
-                    <option value="" disabled selected>Select a category</option>
-                    <option value="electronics">Electronics</option>
-                    <option value="furniture">Furniture</option>
-                    
-                </select>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Add Item</button>
-          </form>
-         </div>
-    </AdminLayout>       
+    </AdminLayout>    
   )
 }
 
-export default Products_Add
+export default Addproduct
